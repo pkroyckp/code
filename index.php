1ca7ce38e4ee9aab7ea60d65c08684b5bc12dbf5
pkroyckp <63915475+pkroyckp@users.noreply.github.com>
2020-04-20 11:31:09 +0530

    update3.6

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ simple
 </title>
 </head>
 <body>
-<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpradipmejharkhand%2Fvideos%2F1904130916385797%2F&width=0" width="0" height="0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-</body>
+  <h1 anlgn="center">St. Xavier's English School, CKP</h1>
+  <h2 anlgn="center">Pradip Kumat Roy</h2>
+  <script id="163051_c_536979" width="590" height="431" src="https://player.dacast.com/js/player.js?contentId=163051_c_536979"  class="dacast-video"></script>
+  </body>
 </html>
 
 
