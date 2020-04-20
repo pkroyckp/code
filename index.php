45d2dd17ec11ca184485556cc69a6a8b40735588
pkroyckp <63915475+pkroyckp@users.noreply.github.com>
2020-04-20 09:39:48 +0530

    update3.6

--- index.php
+++ index.php
@@ -5,66 +5,7 @@ simple
 </title>
 </head>
 <body>
-<script>
-var vidyoConnector;
-function onVidyoClientLoaded(status){
-console.log("VidyoClient load state-"+ status.state);
-if(status.state=="READY")
-{
-VC.CreateVidyoConnector({
-viewId:"renderer",
-viewStyle:"VIDYO_CONNECTORVIEWSTYLE_Default",
-remoteParticipants:80,
-logFileFilter:"error",
-logFileName:"",
-userData:""
-}).then(function (vc)
-{
-console.log("Create success");
-vidyoConnector=vc;
-}).catch(function(error){
-
-
-});
-/* Local camera change initiated by user. Note: this is an arbitrary function name. */
-function handleCameraChange() {
-  /* Hide view of previously selected camera. */
-  vidyoConnector.HideView({
-    viewId: "Div where camera is rendered"
-  });
-  /* Select new camera */
-  vidyoConnector.SelectLocalCamera({
-    localCamera: camera
-  });
-}
-
-}
-}
-function joinCall(){
-vidyoConnector.Connect({
-host:"77242a.vidyo.io",
-token:"cHJvdmlzaW9uAFByYWRpcEA3NzI0MmEudmlkeW8uaW8ANjM3NTQ1MjU5MzMAADUwZjUyMzY0ZmQ0YzUzNDgxM2FjMTcyNGVmMTA4Mzc1OTIwM2Q5YTEyZGM5MTFkMzM5YmE2MjY0MjUxYTc5ODMzNzliMTg5MWViYWUxNTdmN2Q4NThlMmMxMGIzY2NiOA==",
-displayName:"Pradip",
-resourceId:"Class X",
-onSuccess: function()
-{
-console.log("Connected");
-},
-onFailure: function(reason){ 
-console.log("Connection failed");
-},
-onDisconnected: function(reason)
-{
-console.log("disconnected-" +reason);
-}
- })
-}
-</script>
-<script src="https://static.vidyo.io/4.1.8.1/javascript/VidyoClient/VidyoClient.js?onload=onVidyoClientLoaded"></script>
-<h3>
-video</h3>
-<button onClick="joinCall()">Connect</button>
-<div id="renderer"> </div>
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpradipmejharkhand%2Fvideos%2F1904130916385797%2F&width=0" width="0" height="0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
 </body>
 </html>
 
