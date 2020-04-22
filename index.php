6d26d4c85badc373bc84a78f0c1b170fc8f75e71
pkroyckp <63915475+pkroyckp@users.noreply.github.com>
2020-04-22 12:25:21 +0530

    update3.7

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ simple
 <body>
   <h1 anlgn="center">St. Xavier's English School, CKP</h1>
   <h2 anlgn="center">Pradip Kumat Roy</h2>
-  <script id="163051_c_536979" width="590" height="431" src="https://player.dacast.com/js/player.js?contentId=163051_c_536979"  class="dacast-video"></script>
-  </body>
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F109582160727281%2Fvideos%2F672521000201664%2F&width=0" width="390" height="431" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+</body>
 </html>
 
 
