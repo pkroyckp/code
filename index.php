21c36aab8e8c1622be5c7ac9a141c75b61a73856
pkroyckp <63915475+pkroyckp@users.noreply.github.com>
2020-05-05 09:26:24 +0530

    Online Java Editor0.1

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+
+* {
+  box-sizing: border-box;
+}
+.menu {
+  float: left;
+  width: 20%;
+}
+.menuitem {
+  padding: 8px;
+  margin-top: 7px;
+  border-bottom: 1px solid #f1f1f1;
+}
+.main {
+  float: left;
+  width: 60%;
+  padding: 0 20px;
+  overflow: hidden;
+}
+.right {
+  background-color: lightblue;
+  float: left;
+  width: 20%;
+  padding: 10px 15px;
+  margin-top: 7px;
+}
+
+@media only screen and (max-width:800px) {
+  /* For tablets: */
+  .main {
+    width: 80%;
+    padding: 0;
+  }
+  .right {
+    width: 100%;
+  }
+}
+@media only screen and (max-width:500px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width: 100%;
+  }
+}
+html 
+{
+ overflow: auto;
+}
+ 
+html, body, div, iframe 
+{
+ margin: 0px; 
+ padding: 0px; 
+ height: 100%; 
+ border: none;
+}
+iframe 
+{
+ display: block; 
+ width: 100%; 
+ border: none; 
+ overflow-y: auto; 
+ overflow-x: hidden;
+}
+</style>
+</head>
+
+<body style="font-family:Verdana;">
+  <h1>P. K. Roy</h1>
+  <p>Powered by "javatpoint.com"</p>
+
+<iframe frameborder="0" marginwidth="0" marginheight="0" src="https://compiler.javatpoint.com/opr/test.jsp" width="100%" height="100%" scrolling="auto"></iframe>
+
+</body>
+
+</body>
+</html>
+
+
