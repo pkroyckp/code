26ba45ba4b0dd0284e0f61ab99c5e643d689738c
pkroyckp <63915475+pkroyckp@users.noreply.github.com>
2020-05-19 09:48:07 +0530

    up1

--- index.php
+++ index.php
@@ -1,83 +1,174 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
 
-* {
-  box-sizing: border-box;
-}
-.menu {
-  float: left;
-  width: 20%;
-}
-.menuitem {
-  padding: 8px;
-  margin-top: 7px;
-  border-bottom: 1px solid #f1f1f1;
-}
-.main {
-  float: left;
-  width: 60%;
-  padding: 0 20px;
-  overflow: hidden;
-}
-.right {
-  background-color: lightblue;
-  float: left;
-  width: 20%;
-  padding: 10px 15px;
-  margin-top: 7px;
+ <title>Notification using PHP Ajax Bootstrap</title>
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script>
+
+$(document).ready(function(){
+
+// updating the view with notifications using ajax
+
+function load_unseen_notification(view = '')
+
+{
+
+ $.ajax({
+
+  url:"fetch.php",
+  method:"POST",
+  data:{view:view},
+  dataType:"json",
+  success:function(data)
+
+  {
+
+   $('.dropdown-menu').html(data.notification);
+
+   if(data.unseen_notification > 0)
+   {
+    $('.count').html(data.unseen_notification);
+   }
+
+  }
+
+ });
+
 }
 
-@media only screen and (max-width:800px) {
-  /* For tablets: */
-  .main {
-    width: 80%;
-    padding: 0;
-  }
-  .right {
-    width: 100%;
-  }
-}
-@media only screen and (max-width:500px) {
-  /* For mobile phones: */
-  .menu, .main, .right {
-    width: 100%;
-  }
-}
-html 
-{
- overflow: auto;
-}
- 
-html, body, div, iframe 
-{
- margin: 0px; 
- padding: 0px; 
- height: 100%; 
- border: none;
-}
-iframe 
-{
- display: block; 
- width: 100%; 
- border: none; 
- overflow-y: auto; 
- overflow-x: hidden;
-}
-</style>
+load_unseen_notification();
+
+// submit form and get new records
+
+$('#comment_form').on('submit', function(event){
+ event.preventDefault();
+
+ if($('#subject').val() != '' && $('#comment').val() != '')
+
+ {
+
+  var form_data = $(this).serialize();
+
+  $.ajax({
+
+   url:"insert.php",
+   method:"POST",
+   data:form_data,
+   success:function(data)
+
+   {
+
+    $('#comment_form')[0].reset();
+    load_unseen_notification();
+
+   }
+
+  });
+
+ }
+
+ else
+
+ {
+  alert("Both Fields are Required");
+ }
+
+});
+
+// load new notifications
+
+$(document).on('click', '.dropdown-toggle', function(){
+
+ $('.count').html('');
+
+ load_unseen_notification('yes');
+
+});
+
+setInterval(function(){
+
+ load_unseen_notification();;
+
+}, 5000);
+
+});
+
+</script>
+
 </head>
 
-<body style="font-family:Verdana;">
-  <h1 align"center">P. K. Roy</h1><a href="http://4fdbeb45.ngrok.io/videochat/index.php"><button type='submit'>Live</button></a>
-  <p>Powered by "javatpoint.com"</p>
+<body>
 
-<iframe frameborder="0" marginwidth="0" marginheight="0" src="https://compiler.javatpoint.com/opr/test.jsp" width="100%" height="100%" scrolling="auto"></iframe>
+ <br /><br />
+
+ <div class="container">
+
+  <nav class="navbar navbar-inverse">
+
+   <div class="container-fluid">
+
+    <div class="navbar-header">
+
+     <a class="navbar-brand" href="#">PHP Notification Tutorial</a>
+
+    </div>
+
+    <ul class="nav navbar-nav navbar-right">
+
+     <li class="dropdown">
+
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+
+      <ul class="dropdown-menu"></ul>
+
+     </li>
+
+    </ul>
+
+   </div>
+
+  </nav>
+
+  <br />
+
+  <form method="post" id="comment_form">
+
+   <div class="form-group">
+
+    <label>Enter Subject</label>
+
+    <input type="text" name="subject" id="subject" class="form-control">
+
+   </div>
+
+   <div class="form-group">
+
+    <label>Enter Comment</label>
+
+    <textarea name="comment" id="comment" class="form-control" rows="5"></textarea>
+
+   </div>
+
+   <div class="form-group">
+
+    <input type="submit" name="post" id="post" class="btn btn-info" value="Post" />
+
+   </div>
+
+  </form>
+
+
+ </div>
 
 </body>
 
-</body>
 </html>
-
 
